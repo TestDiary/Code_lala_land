160c7e202d09186ed6e3db9e60943e3701aa45bc
Femi <olufemi@clearbooks.co.uk>
2016-03-28 17:20:50 +0100

    Drop downs

---
+++ TestDiary_Learning_Selenium/src/testDiary/TestdiaryDropDowns.java
@@ -0,0 +1,81 @@
+package testDiary;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestdiaryDropDowns {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new FirefoxDriver();
+
+		driver.manage().window().maximize();
+
+		String contactUrl = "http://www.testdiary.com/training/selenium/selenium-test-page/";
+
+		driver.get(contactUrl);
+
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions
+				.presenceOfElementLocated(By.name("your-name")));
+
+		// Select from the drop down by visible text
+		new Select(driver.findElement(By.id("Shirts")))
+				.selectByVisibleText("Red Shirt");
+
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+
+		// Select from the drop down by value
+		new Select(driver.findElement(By.id("Skirts")))
+				.selectByValue("YellowSkirt");
+
+		WebElement name = driver.findElement(By.name("your-name"));
+
+		name.clear();
+
+		name.sendKeys("Femi");
+
+		WebElement email = driver.findElement(By.name("your-email"));
+
+		email.clear();
+
+		email.sendKeys("xxxx@testdiary.com");
+
+		WebElement subject = driver.findElement(By.name("your-subject"));
+		subject.clear();
+		subject.sendKeys("New tester");
+
+		WebElement your_message = driver.findElement(By.name("your-message"));
+		your_message.clear();
+		your_message.sendKeys("I want to be the best tester.");
+
+		WebElement send = driver
+				.findElement(By.xpath("//input[@value='Send']"));
+
+		send.click();
+
+		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+
+		String actual_message = driver.findElement(
+				By.xpath("//div[@id='wpcf7-f551-p683-o1']/form/div[2]"))
+				.getText();
+
+		String expected_message = "Your message was sent successfully. Thanks.";
+
+		if (expected_message.equals(actual_message)) {
+
+			System.out.println("verification successful - " + actual_message);
+		} else {
+			System.out.println("verification unsuccessful");
+		}
+
+		driver.close();
+
+		System.exit(0);
+	}
+}
